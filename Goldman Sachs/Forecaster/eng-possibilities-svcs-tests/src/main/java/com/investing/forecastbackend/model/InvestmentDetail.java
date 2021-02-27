fd34893cac838ad0c30362f7a5717c846d5ad98a
caitmoy <caitlin.moy@outlook.com>
2021-02-27 12:36:10 -0500

    getters and setters

--- Goldman Sachs/Forecaster/eng-possibilities-svcs-tests/src/main/java/com/investing/forecastbackend/model/InvestmentDetail.java
+++ Goldman Sachs/Forecaster/eng-possibilities-svcs-tests/src/main/java/com/investing/forecastbackend/model/InvestmentDetail.java
@@ -13,6 +13,30 @@ public class InvestmentDetail {
     private String category;
     private String minimum;
     private List<String> data;
+
+    public String getCategory(){
+        return this.category;
+    }
+
+    public String getMinimum(){
+        return this.minimum;
+    }
+
+    public List<String> getData(){
+        return this.data;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setMinimum(String minimum) {
+        this.minimum = minimum;
+    }
+
+    public void setData(List<String> data) {
+        this.data = data;
+    }
 }
 
  
