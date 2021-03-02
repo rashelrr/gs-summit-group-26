7ce7d6e1f652cca4491d808df0f9b1040776e821
caitmoy <caitlin.moy@outlook.com>
2021-03-02 17:06:42 -0500

    tried putting a chart in

--- Goldman Sachs/Forecaster/eng-possibilities-svcs-tests/src/main/java/com/investing/UI/src/pages/Forecaster/index.js
+++ Goldman Sachs/Forecaster/eng-possibilities-svcs-tests/src/main/java/com/investing/UI/src/pages/Forecaster/index.js
@@ -1,18 +1,64 @@
 import React from "react";
 import {Container} from "react-bootstrap";
+import {Line} from 'react-chartjs-2';
 
-const ForecasterHome = () => {
-  return (
-    <>
-       <div>
-            <h3>Investment Forecaster</h3>
-            {/* <p>Logic goes here</p> */}
-            <Container>Chart goes here</Container>
+const state = {
+  // TODO import years
+  labels: ['2021', '2022', '2023',
+           '2024', '2025','2026','2027',
+           '2028','2029', '2030'],
+  datasets: [
+    {
+      label: 'Projection',
+      fill: false,
+      lineTension: 0.5,
+      backgroundColor: 'rgba(250,0,0,1)',
+      borderColor: 'rgba(250,0,0,1)',
+      borderWidth: 2,
+      // TODO import data (these are just dummy values)
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Line
+          data={state}
+          options={{
+            title:{
+              display:true,
+              text:'Investment Forecast Over 10 Years',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+
+
+
+// const ForecasterHome = () => {
+//   return (
+//     <>
+//        <div>
+//             <h3>Investment Forecaster</h3>
+//             {/* <p>Logic goes here</p> */}
+//             <Container>Chart goes here</Container>
             
-            <Container>User input here</Container>
-          </div>
-    </>
-  );
-};
+//             <Container>User input here</Container>
+//           </div>
+//     </>
+//   );
+// };
 
-export default ForecasterHome;
+// export default ForecasterHome;
